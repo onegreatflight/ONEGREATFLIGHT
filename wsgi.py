8cf5e75672c81793a04470854be0f8def23abfe2
onegreatflight <onegreatflight@gmail.com>
2025-06-02 17:39:11 -0700

    Add files via upload
    please add complete file

---
+++ wsgi.py
@@ -0,0 +1,25 @@
+import os
+from gunicorn.app.base import BaseApplication
+from src.main import app
+
+class StandaloneApplication(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+if __name__ == '__main__':
+    options = {
+        'bind': '0.0.0.0:' + os.environ.get('PORT', '10000'),
+        'workers': 3,
+    }
+    StandaloneApplication(app, options).run()
